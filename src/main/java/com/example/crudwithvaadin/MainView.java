1eff5bd4c84afba168e0e329828c8282cab0018b
veeruproton <veeruproton>
2025-02-21 04:37:06 +0530

    simpilfy main view

--- src/main/java/com/example/crudwithvaadin/MainView.java
+++ src/main/java/com/example/crudwithvaadin/MainView.java
@@ -1,77 +1,23 @@
 package com.example.crudwithvaadin;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 
 import jakarta.annotation.security.PermitAll;
 
-import org.springframework.util.StringUtils;
-
-@Route
+@Route("/home")
 @PermitAll
 public class MainView extends VerticalLayout {
 
+	public MainView() {
 
-	final Grid<Customer> grid;
+		String userName = "Not Logged In User";
 
-	final TextField filter;
+		add(new H2("Home page"));
+		add(new H3("Welcome " + userName + "!"));
+		add(new H3("You should only see this page if you are logged in"));
 
-	private final Button addNewBtn;
-
-	public MainView(CustomerRepository repo, CustomerEditor editor) {
-
-		this.grid = new Grid<>(Customer.class);
-		this.filter = new TextField();
-		this.addNewBtn = new Button("New customer", VaadinIcon.PLUS.create());
-
-		// build layout
-		HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
-		add(actions, grid, editor);
-
-		grid.setHeight("300px");
-		grid.setColumns("id", "firstName", "lastName");
-		grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
-
-		filter.setPlaceholder("Filter by last name");
-
-		// Hook logic to components
-
-		// Replace listing with filtered content when user changes filter
-		filter.setValueChangeMode(ValueChangeMode.LAZY);
-		filter.addValueChangeListener(e -> listCustomers(repo, e.getValue()));
-
-		// Connect selected Customer to editor or hide if none is selected
-		grid.asSingleSelect().addValueChangeListener(e -> {
-			editor.editCustomer(e.getValue());
-		});
-
-		// Instantiate and edit new Customer the new button is clicked
-		addNewBtn.addClickListener(e -> editor.editCustomer(new Customer("", "")));
-
-		// Listen changes made by the editor, refresh data from backend
-		editor.setChangeHandler(() -> {
-			editor.setVisible(false);
-			listCustomers(repo, filter.getValue());
-		});
-
-		// Initialize listing
-		listCustomers(repo, null);
 	}
-
-	// tag::listCustomers[]
-	void listCustomers(CustomerRepository repo, String filterText) {
-		if (StringUtils.hasText(filterText)) {
-			grid.setItems(repo.findByLastNameStartsWithIgnoreCase(filterText));
-		} else {
-			grid.setItems(repo.findAll());
-		}
-	}
-	// end::listCustomers[]
-
 }
